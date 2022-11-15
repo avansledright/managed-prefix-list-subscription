e601b3b5065baa89fd66618e8c43380caf7a2308
Aaron VanSledright <avansledright@gmail.com>
2022-11-15 14:18:00 -0600

    Pushing final updates

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # managed-prefix-list-subscription
+
+A Python script to automate updating AWS Managed Prefix Lists.
+
+## Usage:
+1. Update the method in which you want to obtain your IPs
+2. Pass the variables into the function.
--- main.py
+++ main.py
@@ -5,6 +5,24 @@ import boto3
 from botocore.exceptions import ClientError
 import ipaddress
 
+def check_for_existing(list_id, ip):
+    client = boto3.client("ec2", region_name="us-west-2")
+    try:
+        response = client.get_managed_prefix_list_entries(
+            PrefixListId=list_id,
+            MaxResults=100,
+        )
+        for entry in response['Entries']:
+            if entry['Cidr'] == ip:
+                return True
+            else:
+                pass
+        return False
+    except ClientError as e:
+        print(e)
+
+
+
 def get_prefix_list_id(list_name):
     client = boto3.client("ec2", region_name="us-west-2")
     response = client.describe_managed_prefix_lists(
@@ -21,40 +39,35 @@ def get_prefix_list_id(list_name):
 
 def update_managed_prefix_list(list_name, ip):
     client = boto3.client("ec2", region_name="us-west-2")
-    try:
-        response = client.modify_managed_prefix_list(
-                    DryRun=False,
-                    PrefixListId=get_prefix_list_id(list_name)['ID'],
-                    CurrentVersion=get_prefix_list_id(list_name)['VERSION'],
-                    AddEntries=[
-                        {
-                            "Cidr": ip
-                        }
-                    ]
-                )
-        return True
-    except ClientError as e:
-        print(e)
-        print("Failed to update list")
-def check_ip_type(list_name, ip_list):
-    try:
-        for ip in ip_list:
-            if type(ipaddress.ip_address(ip[:-3])) is ipaddress.IPv6Address:
-                print(ip)
-                update_managed_prefix_list(list_name, ip)
-            else:
-                update_managed_prefix_list(list_name, ip)
-                print(ip)
-        return True
-    except:
+    if check_for_existing(get_prefix_list_id(list_name)['ID'], ip) == True:
+        print("Rule already exists")
         return False
+    else:
+        try:
+            response = client.modify_managed_prefix_list(
+                        DryRun=False,
+                        PrefixListId=get_prefix_list_id(list_name)['ID'],
+                        CurrentVersion=get_prefix_list_id(list_name)['VERSION'],
+                        AddEntries=[
+                            {
+                                "Cidr": ip
+                            }
+                        ]
+                    )
+            return True
+        except ClientError as e:
+            print(e)
+            print("Failed to update list")
+
 if __name__ == "__main__":
     url = "https://<my IP address URL>"
     headers = {}
     r = requests.get(url, headers=headers)
     json_ips = json.loads(r.content)
-
-    if check_ip_type("hooks", hooks) == True:
-        print("Successfully Added IPs")
+    ip = ""
+    list_name = ""
+    result = update_managed_prefix_list(list_name, ip)
+    if result == True:
+        print("Successfully Updates lists")
     else:
-        print("Failed to add IPs")
+        print("Failed to update lists")
